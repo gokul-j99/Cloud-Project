d872ed129eda2098085c6d6097239086bd001082
Jackpete <=>
2023-09-25 16:02:38 -0400

    added 404 middleware

--- api/Routes/index.js
+++ api/Routes/index.js
@@ -1,4 +1,9 @@
 import baseRouter from './baseRouter.js';
 export default(app) => {
-    app.use('/healtz',baseRouter);
+    app.use('/healtz',baseRouter)
+    
+    app.use((req, res) => {
+        res.set('Cache-Control', 'no-cache');
+        res.status(404).end();
+    });
 }
